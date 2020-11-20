8e2446dd93559e9e3309bd88bc938744e71accdf
Francisco Farias <fariasjunior96@gmail.com>
2020-11-20 20:56:50 -0300

    max keyword count validation

--- src/middlewares/getKeywords.js
+++ src/middlewares/getKeywords.js
@@ -1,7 +1,16 @@
 'use strict';
 
-module.exports = (req, _, next) => {
-  const keywordsAsString = req.query.i || '';
-  req.body.keywords = keywordsAsString.split(',').sort();
+const MAX_KEYWORDS_COUNT = 3;
+const MAX_KEYWORDS_ERROR_MSG = `You can't use more than ${MAX_KEYWORDS_COUNT} keywords.`;
+
+module.exports = (req, res, next) => {
+  const keywords = (req.query.i || '').split(',');
+
+  if (keywords.length > MAX_KEYWORDS_COUNT) {
+    res.status(400).send(MAX_KEYWORDS_ERROR_MSG);
+    return;
+  }
+
+  req.body.keywords = keywords.sort();
   next();
 };
